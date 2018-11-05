cb4ec85a8091932bd4821825ad70fc06e50c13c2
sensible-proc-7557 <vchan313@gmail.com>
2018-11-05 19:21:45 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word (string1)
+  array1 = string1.split(" ")
+  array2 = []
+
+  array1.collect do |word| 
+    array2.push(word.reverse)
+    
+  end
+  string2 = array2.join(" ")
+  
+  
+
+return string2
+end
+
