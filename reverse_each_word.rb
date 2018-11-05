61d9f128c1ef725c5573f06a7b4f9ed8acc2561c
sensible-proc-7557 <vchan313@gmail.com>
2018-11-05 19:21:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,16 @@
-def reverse_each_word()
+def reverse_each_word (string1)
+  array1 = string1.split(" ")
+  array2 = []
+
+  array1.collect do |word| 
+    array2.push(word.reverse)
+    
+  end
+  string2 = array2.join(" ")
   
+  
+
+return string2
 end
+
+reverse_each_word(string1)
